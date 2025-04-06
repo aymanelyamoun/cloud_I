733060325afebcffab19a92418bb3b104f790410
aymanelyamoun <aymanelyamoun93@gmail.com>
2025-04-06 16:09:56 +0100

    changing nginx base image

--- nginx/files/assets/nginx_setup.sh
+++ nginx/files/assets/nginx_setup.sh
@@ -7,9 +7,7 @@ adduser --disabled-password --gecos "" www || true
 chown -R www:www /var/lib/nginx
 mkdir -p /var/www && chown -R www:www /var/www
 
-# Replace default config
-cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-cp /assets/nginx.conf /etc/nginx/conf.d/default.conf
+cp /assets/nginx.conf /etc/nginx/sites-available/default
 
 # adduser -D -g 'www' www
 
