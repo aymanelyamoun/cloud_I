a8452fb0d5d7d6fa14b81fb0b8821aa37c95d5f9
C0M-M4ND0 <omarabdelhadi1337@gmail.com>
2025-04-08 07:53:48 +0100

    remove unused nginx and wordpress role files and playbook

--- roles/mariadb/files/assets/change_root_pass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#mysqld_safe --user=root --datadir=/var/lib/mysql &
-mysql -uroot -p $WORDPRESS_PASS <<EOSQL
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass';
-FLUSH PRIVILEGES;
-EOSQL
--- roles/mariadb/files/assets/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mariadbd --no-defaults --datadir=/var/lib/mysql --user=root --init-file=/assets/init.sql
-#/assets/change_root_pass.sh
